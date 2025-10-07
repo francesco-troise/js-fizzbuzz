3b72d3129a46f4ab80f4e5b11d705306778d2bd6
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-07 13:54:31 +0200

    Add -for loop section
    Add -for loop structure
    
    Initialize "i = 1" in the -for structure to start from 1
    
    Add the "i <= 100" control condition in the -for structure to stop the advancement at 100
    
    Add "i++" in the -for structure to update the loop count

--- assets/main.js/script.js
+++ assets/main.js/script.js
@@ -6,3 +6,10 @@ i numeri multipli solo di 5 ricevono un "badge" -Buzz
 i numeri multipli solo di 3 ricevono un "badge" -Fizz
 i numeri non multipli nè di 3 nè di 5 non ricevono "badge"`)
 //stampa introduzione
+
+/*SEZIONE CICLO FOR*/
+
+for(i = 1; i <= 100; i++){
+//imposto la struttura del ciclo for(-i parte da 1 ed arriverà fino a 100)
+
+

30ea5656976ae8be0a4e3033c25cd1b2ef8036c5
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-07 13:52:59 +0200

    Add Sezione introduttiva
     Add -alert to print intro on screen

---
+++ assets/main.js/script.js
@@ -0,0 +1,8 @@
+/*INTRODUZIONE PER L'UTENTE*/
+
+alert(`Il programma stamperà a schermo i numeri da 1 a 100
+i numeri multipli sia di 5 che di 3 ricevono un "badge" -FizzBuzz
+i numeri multipli solo di 5 ricevono un "badge" -Buzz
+i numeri multipli solo di 3 ricevono un "badge" -Fizz
+i numeri non multipli nè di 3 nè di 5 non ricevono "badge"`)
+//stampa introduzione
